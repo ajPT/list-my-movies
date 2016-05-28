22026f95190004ddabb3950e8998e7f96a55b96a
Amadeu Andrade <amadeuandrade@Amadeus-MBP.home>
2016-05-28 21:46:01 +0100

    Updated Search View

--- my-favorite-movies/SearchVC.swift
+++ my-favorite-movies/SearchVC.swift
@@ -12,37 +12,28 @@ class SearchVC: UIViewController {
 
     @IBOutlet weak var selectAnOptionLbl: UILabel!
     @IBOutlet weak var magnifierImg: UIImageView!
-    @IBOutlet weak var searchByLbl: UILabel!
     @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var yearField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBarHidden = false
-        searchByLbl.hidden = true
         searchField.hidden = true
         yearField.hidden = true
     }
 
-    @IBAction func onSearchByTitlePressed(sender: UIButton) {
+    @IBAction func onSearchByPressed(sender: UIButton) {
         selectAnOptionLbl.hidden = true
-        magnifierImg.hidden = true
-        searchByLbl.hidden = false
-        searchByLbl.text = "Insert movie title:"
+        magnifierImg.alpha = 0.25
         searchField.hidden = false
-        searchField.placeholder = "Title"
-        yearField.hidden = false
-        yearField.placeholder = "Year?"
-    }
-    
-    @IBAction func onSearchByIDPressed(sender: UIButton) {
-        selectAnOptionLbl.hidden = true
-        magnifierImg.hidden = true
-        searchByLbl.hidden = false
-        searchByLbl.text = "Insert IMDd ID:"
-        searchField.hidden = false
-        searchField.placeholder = "IMDb ID"
-        yearField.hidden = true
+        if sender.tag == 0 {
+            searchField.placeholder = "Title"
+            yearField.hidden = false
+            yearField.placeholder = "Year?"
+        } else if sender.tag == 1 {
+            searchField.placeholder = "IMDb ID"
+            yearField.hidden = true
+        }
     }
     
     @IBAction func onSearchPressed(sender: UIButton) {

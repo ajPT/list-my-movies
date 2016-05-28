e0f4b511a179e4056483c437338887286bc57d66
Amadeu Andrade <amadeuandrade@Amadeus-MBP.home>
2016-05-28 18:46:18 +0100

    changed layout

---
+++ my-favorite-movies/RoundedCornerBtn.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedCornerBtn.swift
+//  my-favorite-movies
+//
+//  Created by Amadeu Andrade on 28/05/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class RoundedCornerBtn: UIButton {
+    
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+    }
+    
+}
--- my-favorite-movies/SearchVC.swift
+++ my-favorite-movies/SearchVC.swift
@@ -13,7 +13,7 @@ class SearchVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.navigationBarHidden = false
     }
 
 
